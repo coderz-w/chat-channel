8280761b7566bad0fffe32456029286880c61444
zhwcreate <3331598351@qq.com>
2024-02-14 11:19:17 +0800

    feat: clerk

---
+++ chat-channel/app/(auth)/layout.tsx
@@ -0,0 +1,5 @@
+const AuthLayout = ({ children }: { children: React.ReactNode }) => {
+  return <div className=" h-ful flex items-center justify-center">{children}</div>;
+};
+
+export default AuthLayout;
