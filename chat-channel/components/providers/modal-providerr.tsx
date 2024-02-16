eb7714c3f62ac1176bdf963b021d58df7bb06ea1
zhwcreate <3331598351@qq.com>
2024-02-16 17:09:47 +0800

    feat: crate channel

--- chat-channel/components/providers/modal-providerr.tsx
+++ chat-channel/components/providers/modal-providerr.tsx
@@ -6,6 +6,7 @@ import { CreateServerModal } from "@/components/modals/create-server-modal";
 import { InviteModal } from "../modals/invite-modal";
 import { EditServerModal } from "../modals/edit-server-modal";
 import { MembersModal } from "../modals/members-modal";
+import { CreateChannelModal } from "../modals/create-channel-modal";
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false);
@@ -22,8 +23,9 @@ export const ModalProvider = () => {
     <>
       <CreateServerModal />
       <InviteModal />
-      <EditServerModal/>
-      <MembersModal/>
+      <EditServerModal />
+      <MembersModal />
+      <CreateChannelModal />
     </>
   );
 };
