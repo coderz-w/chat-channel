404529659b402622819e41ae4e096c01f152e74a
zhwcreate <3331598351@qq.com>
2024-02-18 17:12:50 +0800

    feat prisma complete

--- chat-channel/app/(main)/(routes)/servers/[serverid]/page.tsx
+++ chat-channel/app/(main)/(routes)/servers/[serverid]/page.tsx
@@ -1,5 +1,50 @@
-const ServerIdPage = () => {
-  return <div>server ID Page</div>;
-};
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+interface ServerIdPageProps {
+  params: {
+    serverid: string;
+  };
+}
+
+const ServerIdPage = async ({ params }: ServerIdPageProps) => {
+  const profile = await currentProfile();
+
+  if (!profile) {
+    return redirectToSignIn();
+  }
+
+  const server = await db.server.findUnique({
+    where: {
+      id: params.serverid,
+      Members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+    include: {
+      Channels: {
+        where: {
+          name: "general",
+        },
+        orderBy: {
+          createdAt: "asc",
+        },
+      },
+    },
+  });
+  console.log(server);
+
+  const initialChannel = server?.Channels[0];
+
+  if (initialChannel?.name !== "general") {
+    return null;
+  }
+
+  return redirect(`/servers/${params.serverid}/channels/${initialChannel?.id}`);
+};
 export default ServerIdPage;
