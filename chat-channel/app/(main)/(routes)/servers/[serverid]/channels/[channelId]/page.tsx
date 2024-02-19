c1afd437560b9e99ba12001c9f33b3d4a05dfecf
zhwcreate <3331598351@qq.com>
2024-02-19 15:54:48 +0800

    feat: input component

--- chat-channel/app/(main)/(routes)/servers/[serverid]/channels/[channelId]/page.tsx
+++ chat-channel/app/(main)/(routes)/servers/[serverid]/channels/[channelId]/page.tsx
@@ -5,6 +5,8 @@ import { currentProfile } from "@/lib/current-profile";
 import { ChatHeader } from "@/components/chat/chat-header";
 import { db } from "@/lib/db";
 
+import { ChatInput } from "@/components/chat/chat-input";
+
 interface ChannelIdPageProps {
   params: {
     serverId: string;
@@ -43,7 +45,17 @@ const ChannelIdPage = async ({ params }: ChannelIdPageProps) => {
         serverId={channel.serverId}
         type="channel"
       />
+      <div className=" flex-1">Future Messages</div>
+      <ChatInput
+        name={channel.name}
+        type="channel"
+        apiUrl="/api/socket/messages"
+        query={{
+          channelId: channel.id,
+          serverId: channel.serverId,
+        }}
+      ></ChatInput>
     </div>
   );
 };
-export default ChannelIdPage
+export default ChannelIdPage;
