39d7905d29ce34a5ad966979880c7c4fc56f97b8
zhwcreate <3331598351@qq.com>
2024-02-21 00:01:57 +0800

    feat: chat-item

--- chat-channel/components/chat/chat-messages.tsx
+++ chat-channel/components/chat/chat-messages.tsx
@@ -3,10 +3,12 @@
 import { Fragment } from "react";
 import { Member, Message, Profile } from "@prisma/client";
 import { Loader2, ServerCrash } from "lucide-react";
+import { format } from "date-fns";
 
 import { useChatQuery } from "@/hooks/use-chat-query";
 
 import { ChatWelcome } from "./chat-welcome";
+import { ChatItem } from "./chat-item";
 
 type MessageWithMemberWithProfile = Message & {
   member: Member & {
@@ -26,6 +28,7 @@ interface ChatMessagesProps {
   type: "channel" | "conversation";
 }
 
+const DATE_FORMAT = "d MMM yyyy, HH:mm";
 export const ChatMessages = ({
   name,
   member,
@@ -85,9 +88,18 @@ export const ChatMessages = ({
         {data?.pages?.map((group, i) => (
           <Fragment key={i}>
             {group.items.map((message: MessageWithMemberWithProfile) => (
-              <div key={message.id}>
-                {message.content}
-              </div>
+              <ChatItem
+              key={message.id}
+                id={message.id}
+                currentMember={member}
+                member={message.member}
+                content={message.content}
+                fileUrl={message.fileUrl}
+                deleted={message.deleted}
+                timestamp={format(new Date(message.createdAt), DATE_FORMAT)}
+                isUpdated={message.updatedAt !== message.createdAt}
+                socketUrl={socketUrl}
+                socketQuery={socketQuery} />
             ))}
           </Fragment>
         ))}
